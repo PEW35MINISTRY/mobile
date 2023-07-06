aba08a7b3458a506d09e483a26f684f1748ab589
bohnerjosh <bohnerjosh2022@gmail.com>
2023-07-05 20:11:55 -0500

    Implemented refactor for Edit profile and Signup

---
+++ src/1-Profile/Edit-Profile.tsx
@@ -0,0 +1,211 @@
+import React, {useState, useEffect, useContext, useRef} from 'react';
+import {View, Text, Image, StyleSheet, GestureResponderEvent, ScrollView } from 'react-native';
+import axios from 'axios';
+import { DOMAIN } from '@env';
+
+import theme, {COLORS} from '../theme';
+import { useAppSelector, useAppDispatch } from '../TypesAndInterfaces/hooks';
+import { ProfileResponse, Props } from '../TypesAndInterfaces/profile-types';
+import { EDIT_PROFILE_FIELDS, InputType } from '../TypesAndInterfaces/profile-field-config';
+
+import store from '../redux-store';
+import PEW35 from '../../assets/pew35-logo.png';
+import HANDS from '../../assets/hands.png';
+
+import { Flat_Button, Icon_Button, Input_Field, Outline_Button, Raised_Button } from '../widgets';
+import { updateProfile, RootState } from '../redux-store';
+import { useForm, Controller } from "react-hook-form"
+
+// valid password requrements: One uppercase, one lowercase, one digit, one special character, 8 chars in length
+//const validPasswordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+
+const EditProfile = ({navigation}:Props):JSX.Element => {
+    const dispatch = useAppDispatch();
+
+    const jwt = useAppSelector((state: RootState) => state.account.JWT);
+    const userID = useAppSelector((state: RootState) => state.account.userId);
+    var userProfile = store.getState().account.userProfile;
+
+    useEffect(() => {
+      userProfile = store.getState().account.userProfile;
+    }, [])
+
+    console.log(userProfile, jwt, userID);
+    const axiosHeaderData = {
+      headers: {
+        "jwt": jwt, 
+        "user-id": userID,
+      }
+    }
+    
+    // store user profile data as an indexable object
+    const userProfileValues: Record<string, string> = {};
+    for (const [key, value] of Object.entries(userProfile)) {
+      userProfileValues[key] = value;
+    }
+
+    // assign user profile data as default values for fields
+    const inputFieldJSON: Record<string, string> = {};
+    EDIT_PROFILE_FIELDS.forEach((field) => {
+      if (userProfileValues[field.field]) inputFieldJSON[field.field] = userProfileValues[field.field];
+    })
+
+    const {
+      control,
+      handleSubmit,
+      formState: { errors },
+      clearErrors,
+    } = useForm({
+      defaultValues: inputFieldJSON
+    });
+
+    const onEditFields = (fieldData:any):void => {
+
+      // filter out fields that didn't change
+      const editedFields: Record<string, string> = {};
+      for (const [key, value] of Object.entries(fieldData)) {
+        if (value !== userProfileValues[key]) editedFields[key] = value;
+      }
+
+      // send data to server
+      axios.patch(`${DOMAIN}/api/user/profile/` + userProfile.userId, editedFields, axiosHeaderData,
+        ).then(response => {
+            console.log("Profile edit success.", response.data);
+            dispatch(updateProfile({
+              userProfile: response.data.userProfile,
+            }));
+      }).catch(err => console.error("Failed to edit", err))
+      
+      navigation.pop();
+    }
+
+    const renderInputFields = () => {
+      var renderFields:JSX.Element[] = [];
+      EDIT_PROFILE_FIELDS.forEach((field, index) => {
+        switch(field.type) {
+          case InputType.TEXT || InputType.NUMBER:
+            renderFields.push(
+              <Controller 
+                control={control}
+                rules={{
+                  required: field.required,
+                  pattern: field.validationRegex
+                }}
+                render={({ field: {onChange, onBlur, value}}) => (
+                  <Input_Field 
+                    placeholder={field.title}
+                    value={value}
+                    onChangeText={onChange}
+                    keyboardType={(field.type === InputType.NUMBER && "numeric") || "default"}
+                    labelStyle={(errors[field.field] && {color: COLORS.primary}) || undefined}
+                    inputStyle={(errors[field.field] && {borderColor: COLORS.primary}) || undefined}
+                    label={(errors[field.field] && field.validationMessage) || undefined}
+                  />
+                )}
+                name={field.field}
+                key={field.field}
+              />
+            );
+            break;
+          case InputType.PASSWORD:
+            renderFields.push(
+              <Controller 
+                control={control}
+                rules={{
+                  pattern: field.validationRegex,
+                  validate: (value, formValues) => {
+                      if (field.field === "passwordVerify") {
+                        if (value == formValues["password"]) return true;
+                        else return false;
+                      }
+                      else {
+                        return true;
+                      }
+                  }
+                  
+                }}
+                render={({ field: {onChange, onBlur, value}}) => (
+                  <Input_Field 
+                    placeholder={field.title}
+                    value={value}
+                    onChangeText={onChange}
+                    keyboardType='default'
+                    textContentType='password'
+                    labelStyle={(errors[field.field] && {color: COLORS.primary}) || undefined}
+                    inputStyle={(errors[field.field] && {borderColor: COLORS.primary}) || undefined}
+                    label={(errors[field.field] && field.validationMessage) || undefined}
+                  />
+                )}
+                name={field.field}
+                key={field.field}
+              />
+          );
+          break;
+        }
+      })
+      return renderFields;
+    } 
+    return (
+      <View style={styles.center}>
+        <View style={theme.background_view}>
+            <Text style={styles.header}>Edit Profile</Text>
+            <ScrollView>
+              {renderInputFields()}
+            </ScrollView>
+            <Raised_Button buttonStyle={styles.sign_in_button}
+                text='Edit Profile'
+                onPress={handleSubmit(onEditFields)}
+            />
+            <Image source={PEW35} style={styles.pew35_logo}></Image>
+            <Image source={HANDS} style={styles.hands_image} resizeMode='contain'></Image>
+        </View>
+      </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+  ...theme,
+  header: {
+    ...theme.header,
+    marginVertical: 20,
+  },
+  logo: {
+    height: 175,
+    marginBottom: 10,
+  },
+  pew35_logo: {
+    height: 75,
+    width: 75,
+    bottom: 0,
+  },
+  social_icon: {
+    width: 35,
+    height: 35,
+    marginHorizontal: 15,
+  },
+  hands_image: {
+    position: 'absolute',
+    bottom: 0,
+    zIndex: -1,
+    opacity: 0.6
+  },
+  sign_in_button: {
+    marginVertical: 15,
+  },
+  dropdownText: {
+    color: COLORS.white,
+  },
+  dropdownSelected: {
+    color: COLORS.white,
+  },
+  dropdown: {
+    width: 300,
+    marginLeft: 3,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  }
+
+});
+
+export default EditProfile;
--- src/widgets.tsx
+++ src/widgets.tsx
@@ -4,6 +4,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text, TextInput, GestureResponderEvent, Image, ViewStyle, ColorValue, KeyboardTypeOptions, TextStyle, ImageSourcePropType, ImageStyle } from "react-native";
 import theme, {COLORS, FONTS, FONT_SIZES, RADIUS} from './theme';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import DateTimePickerModal, { ReactNativeModalDateTimePickerProps } from "react-native-modal-datetime-picker";
+import { SelectList } from 'react-native-dropdown-select-list'
 
 export const Flat_Button = (props:{text:string|JSX.Element, buttonStyle?:ViewStyle, textStyle?:TextStyle, onPress:((event: GestureResponderEvent) => void)}):JSX.Element => {
 
@@ -67,15 +70,15 @@ export const Outline_Button = (props:{text:string|JSX.Element, buttonStyle?:View
     return ( <Flat_Button {...props} buttonStyle={styles.buttonStyle}/> );    
 }
 
-export const Input_Field = (props:{label?:string|JSX.Element, inputStyle?:TextStyle, labelStyle?:TextStyle, containerStyle?:ViewStyle, 
+export const Input_Field = (props:{label?:string|JSX.Element, inputStyle?:TextStyle, labelStyle?:TextStyle, containerStyle?:ViewStyle,
     value:string, onChangeText:((text: string) => void), placeholder?:string, placeholderTextColor?:ColorValue, keyboardType?:KeyboardTypeOptions, textContentType?:any}):JSX.Element => {  
 
-    const [labelColor, setLabelColor] = useState<ColorValue>(COLORS.transparentWhite);
+    const [labelColor, setLabelColor] = useState(COLORS.accent);
         
     const styles = StyleSheet.create({
         labelStyle: {
             ...theme.accent,
-            color: labelColor,
+            color: COLORS.transparentWhite,
             textAlign: 'left',
             ...props.labelStyle,
         },
@@ -119,4 +122,86 @@ export const Icon_Button = (props:{source:ImageSourcePropType, imageStyle:ImageS
     return ( <TouchableOpacity onPress={props.onPress}>
                  <Image source={props.source} style={props.imageStyle} />
              </TouchableOpacity> );
+}
+
+export const DOBPicker = (props:{label?:string|JSX.Element, buttonStyle?:ViewStyle, buttonText:string, onConfirm:((date:Date) => void) }):JSX.Element => {
+    const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+
+    const styles = StyleSheet.create({
+        containerStyle: {
+            marginVertical: 5,
+        },
+        labelStyle: {
+            ...theme.accent,
+            color: COLORS.primary,
+            textAlign: 'left',
+        }
+    });
+
+    return (
+        <View>
+            <View style={styles.containerStyle}>
+                {props.label && <Text style={styles.labelStyle}>{props.label}</Text>}
+                <Outline_Button
+                    text={props.buttonText}
+                    buttonStyle={props.buttonStyle}
+                    onPress={(event:GestureResponderEvent) => setDatePickerVisible(!isDatePickerVisible)}
+                />
+            </View>
+            <View style={styles.containerStyle}>
+                <DateTimePickerModal 
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={props.onConfirm}
+                    onCancel={(event:Date) => setDatePickerVisible(!isDatePickerVisible)}
+                />
+            </View>
+        </View>
+    )
+}
+
+export const Dropdown_Select = (props:{label?:string, setSelected:((val:string) => void), data: any, placeholder?:string, boxStyle?:ViewStyle}):JSX.Element => {
+    const styles = StyleSheet.create({
+        dropdownText: {
+            color: COLORS.white,
+            textAlign: "center",
+        },
+        dropdownSelected: {
+            color: COLORS.white,
+            textAlign: "center",
+        },
+        dropdown: {
+            width: 300,
+            marginLeft: 3,
+            paddingVertical: 5,
+            paddingHorizontal: 15,
+        },
+        containerStyle: {
+            marginVertical: 5,
+        },
+        errorTextStyle: {
+            ...theme.accent,
+            color: COLORS.primary,
+            textAlign: "left",
+            marginBottom: 5,
+        },
+          
+    });
+    
+    return (
+        <View style={styles.containerStyle}>
+            {props.label && <Text style={styles.errorTextStyle}>{props.label}</Text>}
+            <SelectList 
+                setSelected={(val: string) => props.setSelected(val)} 
+                data={props.data}
+                save="value"
+                boxStyles={props.boxStyle} 
+                dropdownTextStyles={styles.dropdownText}
+                inputStyles={styles.dropdownSelected}
+                placeholder={props.placeholder} 
+             />
+        </View>
+        
+      )
+   
 }
