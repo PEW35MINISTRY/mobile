f66d20984ca2cda91f4d20dea4d6ed2ab4dd5485
bohnerjosh <bohnerjosh2022@gmail.com>
2024-05-21 21:34:01 -0500

    Added validation prop to enable/disable unique field validation

--- src/Widgets/FormInput/form-input-types.tsx
+++ src/Widgets/FormInput/form-input-types.tsx
@@ -7,5 +7,6 @@ export type FormSubmit = {
 export type FormInputProps = {
     fields:InputField[], 
     defaultValues?:any
+    validateUniqueFields?:boolean,
     onSubmit:((formValues:Record<string, string | string[]>) => void)
 }
