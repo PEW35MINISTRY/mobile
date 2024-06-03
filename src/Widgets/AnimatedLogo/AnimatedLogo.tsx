f5b08e8b2da633355a7bf880990e72ed0b418cf3
bohnerjosh <bohnerjosh2022@gmail.com>
2024-06-02 20:23:49 -0500

    Implemented widget for Logo Animation upon login

---
+++ src/Widgets/AnimatedLogo/AnimatedLogo.tsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity, View, Image } from "react-native"
+import React, { useEffect, useRef, useState } from 'react';
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+//@ts-ignore - the file exists
+import PEW_35_ANIMATED_LOGO from '../../../assets/Mobile-Persist-600px-GIF.gif';
+import { ROUTE_NAMES } from "../../TypesAndInterfaces/routes";
+import { StackNavigationProps } from "../../TypesAndInterfaces/custom-types";
+import { StackActions } from '@react-navigation/native';
+
+export const AnimatedLogo = (props:StackNavigationProps):JSX.Element => {
+
+    const [timeoutID, setTimeoutID] = useState<NodeJS.Timeout>(setTimeout(() => props.navigation.dispatch(StackActions.replace(ROUTE_NAMES.BOTTOM_TAB_NAVIGATOR_ROUTE_NAME)), 9000));
+
+    return (
+        <View style={{flex: 1, backgroundColor: "black"}}>
+            <TouchableOpacity
+                onPress={() => {clearTimeout(timeoutID); props.navigation.dispatch(StackActions.replace(ROUTE_NAMES.BOTTOM_TAB_NAVIGATOR_ROUTE_NAME))}} // cancel the timeout and navigate
+                style={{justifyContent: "center", alignSelf: "center", alignContent: "center", top: 30}}
+            >
+                <Image source={PEW_35_ANIMATED_LOGO} resizeMode={"contain"} style={{maxHeight: '90%', maxWidth: '90%'}}/>
+            </TouchableOpacity>
+        </View>
+    )
+}
