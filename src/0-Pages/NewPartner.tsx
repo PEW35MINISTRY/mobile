4baa8a51ecbfac1531d07f14fa9f25070eff92a3
“bohnerjosh” <joshuabx443@proton.me>
2024-11-11 19:57:44 -0600

    Added NewPartner page

---
+++ src/0-Pages/NewPartner.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Modal, TouchableOpacity, SafeAreaView, Platform } from "react-native";
+import { PartnerListItem } from "../TypesAndInterfaces/config-sync/api-type-sync/profile-types";
+import { useAppDispatch, useAppSelector } from "../TypesAndInterfaces/hooks";
+import { addPartnerPendingPartner, RootState, setSettings } from "../redux-store";
+import { PartnerStatusEnum } from "../TypesAndInterfaces/config-sync/input-config-sync/profile-field-config";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { DOMAIN } from "@env";
+import { ServerErrorResponse } from "../TypesAndInterfaces/config-sync/api-type-sync/utility-types";
+import ToastQueueManager from "../utilities/ToastQueueManager";
+import { PartnershipContractModal } from "../4-Partners/partnership-widgets";
+import { RootSiblingParent } from 'react-native-root-siblings';
+import WalkLevelQuiz from "../Widgets/WalkLevelQuiz/WalkLevelQuiz";
+
+const NewPartner = (props:{callback?:((val:number) => void), continueNavigation?:boolean}):JSX.Element => {
+
+    const dispatch = useAppDispatch();
+    const jwt = useAppSelector((state: RootState) => state.account.jwt);
+    const userID = useAppSelector((state: RootState) => state.account.userID);
+    const settingsRef = useAppSelector((state:RootState) => state.settings);
+
+    const [requestNewPartnerModalVisible, setRequestNewPartnerModalVisible] = useState(false);
+
+    const [newPartner, setNewPartner] = useState<PartnerListItem>({
+        status: PartnerStatusEnum.FAILED,
+        userID: -1,
+        firstName: "",
+        displayName: ""
+    } as PartnerListItem);
+
+    const RequestAccountHeader = {
+        headers: {
+          "jwt": jwt,
+        }
+    }
+
+    const POST_NewPartner = (callbackState:number) => {
+        if (callbackState < 0 && props.callback !== undefined) {props.callback(-1); return} 
+
+        axios.post(`${DOMAIN}/api/user/` + userID + '/new-partner', {}, RequestAccountHeader).then((response:AxiosResponse) => {
+            setNewPartner(response.data as PartnerListItem);
+            setRequestNewPartnerModalVisible(true);
+        }).catch((error:AxiosError<ServerErrorResponse>) => ToastQueueManager.show({error}));
+    }
+
+    const acceptPartnershipRequest = () => {
+        axios.post(`${DOMAIN}/api/partner-pending/`+ newPartner.userID + '/accept', {}, RequestAccountHeader).then((response:AxiosResponse) => {
+            dispatch(addPartnerPendingPartner(newPartner));
+            const newStorageState = {...settingsRef, lastNewPartnerRequest: Date.now()}
+            dispatch(setSettings(newStorageState));
+
+            setRequestNewPartnerModalVisible(false);
+            props.callback !== undefined && props.callback(1);
+        }).catch((error:AxiosError<ServerErrorResponse>) => ToastQueueManager.show({error}));
+    }
+
+    const declinePartnershipRequest = () => {
+        axios.delete(`${DOMAIN}/api/partner-pending/`+ newPartner.userID + '/decline', RequestAccountHeader).then((response:AxiosResponse) => {
+            setRequestNewPartnerModalVisible(false);
+            props.callback !== undefined && props.callback(1);
+        }).catch((error:AxiosError<ServerErrorResponse>) => ToastQueueManager.show({error}));
+    }
+
+    return (
+        
+        <SafeAreaView style={{flex: 1, height: '100%'}}>
+            <RootSiblingParent>
+                <WalkLevelQuiz callback={POST_NewPartner}/>
+                <PartnershipContractModal
+                    visible={requestNewPartnerModalVisible}
+                    partner={newPartner}
+                    acceptPartnershipRequest={() => acceptPartnershipRequest()}
+                    declinePartnershipRequest={() => declinePartnershipRequest()}
+                    onClose={() => setRequestNewPartnerModalVisible(false)}
+                />
+            </RootSiblingParent>
+        </SafeAreaView>
+
+
+
+    )
+}
+
+export default NewPartner
